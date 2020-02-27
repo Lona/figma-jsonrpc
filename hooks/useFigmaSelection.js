55d08da5be4214fcc94fdc22a5ebc56469ec60b0
Mathieu Dutour <mathieu@dutour.me>
2020-02-27 18:04:05 +0100

    more efficient hook

--- hooks/useFigmaSelection.js
+++ hooks/useFigmaSelection.js
@@ -1,7 +1,7 @@
-const { useEffect, useState } = require("react");
-const { createUIAPI } = require("../index");
+const { useEffect, useState, useCallback } = require("react");
+const { createUIAPI, createPluginAPI } = require("../index");
 
-const api = createUIAPI({
+const uiApi = createUIAPI({
   __onSelectionChange(selection) {
     if (typeof window.__onSelectionChange !== "undefined") {
       window.__onSelectionChange(selection);
@@ -9,17 +9,27 @@ const api = createUIAPI({
   }
 });
 
-if (typeof figma !== "undefined") {
-  figma.on("selectionchange", () => {
-    api.__onSelectionChange(figma.currentPage.selection);
-  });
-}
+const selectionChangeHandler = () => {
+  uiApi.__onSelectionChange(figma.currentPage.selection);
+};
 
-const listeners = [];
+const api = createPluginAPI({
+  __registerForSelectionChange() {
+    figma.on("selectionchange", selectionChangeHandler);
+  },
+  __deregisterForSelectionChange() {
+    figma.off("selectionchange", selectionChangeHandler);
+  },
+  __setSelection(newSelection) {
+    figma.currentPage.selection = newSelection;
+  }
+});
+
+let listeners = [];
 
 if (typeof window !== "undefined") {
-  window.__onSelectionChange = function(selection) {
-    listeners.forEach(function(l) {
+  window.__onSelectionChange = selection => {
+    listeners.forEach(l => {
       l(selection);
     });
   };
@@ -29,13 +39,21 @@ module.exports = function useFigmaSelection() {
   const [selection, setSelection] = useState([]);
 
   useEffect(() => {
+    if (!listeners.length) {
+      // if it's the first listener, let's register for selection change
+      api.__registerForSelectionChange();
+    }
     listeners.push(setSelection);
     return () => {
-      listeners = listeners.filter(function(l) {
-        return l !== setSelection;
-      });
+      listeners = listeners.filter(l => l !== setSelection);
+      if (!listeners.length) {
+        // if it was the last listener, then we don't have to listen to selection change anymore
+        api.__deregisterForSelectionChange();
+      }
     };
   }, []);
 
-  return selection;
+  const setFigmaSelection = useCallback(api.__setSelection, []);
+
+  return [selection, setFigmaSelection];
 };
